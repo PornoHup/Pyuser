cbccff340cf18d0e9934cc5fd23287d77081d78e
PornoHup <107538335+PornoHup@users.noreply.github.com>
2024-09-09 00:30:32 +0400

    Update userator.py

--- userbot/modules/userator.py
+++ userbot/modules/userator.py
@@ -6,4 +6,4 @@ from userbot.events import register
 @register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.ualive$")
 @register(incoming=True, from_users=WHITELIST, pattern="^.ualive$")
 async def _(q):
-    await q.client.send_message(q.chat_id,"`U S Σ R Δ T O R ✨ Online`")
+    await q.client.send_message(q.chat_id,"`𝐍ə𝐳𝐫𝐢𝐧 𝐔𝐬𝐞𝐫𝐛𝐨𝐭 ✨ Online`")
